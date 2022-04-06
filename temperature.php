618e0a17d011c64dc84b89aa1ab0987d5690a901
Jonasson Oscar <oscjjonasson@gmail.com>
2022-04-06 18:54:45 +0300

    new and improved

--- temperature.php
+++ temperature.php
@@ -1,13 +1,13 @@
 <?php
-$celsius= isset($_GET['celsius'])? $_GET['celsius']: 0;
+$celsius=isset($_GET['celsius'])? $_GET['celsius']: 0;
 ?>
 
 <h2>Temperature conversions: </h2>
 <form>
     <label for="celsius">Celsius: </label>
-    <input type="number" id="celsius" name="celsius" placeholder="Current temp is 0 °c"/>
-    <input type="submit" onclick="celsiusConversion()">
+    <input type="number" id="celsius" name="celsius" placeholder="Current temp is 0 °c" method="GET" action="temperature.php"/>
+    <input type="submit">
 </form>
-<div class="celsius"><?="Current celcius is: ". $celsius . " °c"?></div>
+<div class="celsius"><?="Current celsius is: ". $celsius . " °c"?></div>
 <div id="kelvin">Kelvin: <?= $celsius + 273.15 . " K" ?></div>
 <div class="fahrenheit">Fahrenheit: <?=(($celsius * 1.8) + 32)." °F"?></div>
