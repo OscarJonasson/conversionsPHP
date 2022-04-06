ffa8e90e4fbd2e04315fdb43db0267a1d0531445
MikkeBee <mjblakley87@gmail.com>
2022-04-06 18:51:29 +0300

    mass template progress

--- mass.php
+++ mass.php
@@ -1,1 +1,29 @@
-<div>Eli tää toimis näin</div>
+<?php
+$grams;
+$kilograms;
+
+function toGrams(float $kilograms){
+    $grams = $kilograms / 1000;
+    return $grams;
+}
+
+function toKilos(float $grams){
+    $kilograms = $grams * 1000;
+    return $kilograms;
+}
+
+?>
+
+<div>
+    <h2>Mass Conversion</h2>
+        <div>
+            <label for="kilograms">Kilograms</label>
+            <input type="number" id="kilograms"/>
+            <p><?php echo $_POST["name"]; ?></p>
+        </div>
+        <div>
+            <label for="grams">Grams</label>
+            <input type="number" id="grams"/>
+            <p></p>
+        </div>
+</div>
