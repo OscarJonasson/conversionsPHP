068c178f2a9de39bbd954cd061815e06fa8f565e
Jonasson Oscar <oscjjonasson@gmail.com>
2022-04-06 11:48:49 +0300

    structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversions</title>
+</head>
+<body>
+    <div class="temperature"><?php include 'temperature.php' ?></div>
+    <div class="speed"><?php include 'speed.php' ?></div>
+    <div class="Mass"><?php include 'mass.php' ?></div>
+    
+</body>
+</html>
